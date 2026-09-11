93b9a7be06b656dc2306e7f3aa37db5a99057efe
Sarahn108 <sarahnicholl@hotmail.com>
2026-09-11 14:16:12 +0100

    Change Piece component to use 'contain' for fill mode

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -239,7 +239,7 @@ function SectionBlock({ section, onImageClick }: { section: Section; onImageClic
                       if (photoSrc) onImageClick?.(photoSrc)
                     }}
                       >
-                    <Piece media={it.media} fill fill />
+                    <Piece media={it.media} fill contain />
                   </div>
                 )
               })()}
