2a284b2b668250e262f85c9f1802b25b73fb584a
Sarahn108 <sarahnicholl@hotmail.com>
2026-09-19 18:03:18 +0100

    Update onClick handler for image click events

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -431,7 +431,7 @@ if (section.kind === 'installationViews') {
   key={item.idx}
   className="installation-view-image"
   style={{ cursor: 'pointer' }}
-  onClick={() => onImageClick?.(item.media.src)}
+ onClick={item.media.type === 'photo' ? () => onImageClick?.(item.media.src) : undefined}
 >
   <Piece media={item.media} fill />
 </div>
